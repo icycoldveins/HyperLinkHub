1748ad86f3e0ab17697a06176bd23bab52c0d9da
Kevin Wijaya <kevinwijaya306@gmail.com>
2023-12-29 07:32:42 -0500

    edited readme

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,5 +1,5 @@
 export const AppConfig = {
-  site_name: 'LinkTreeClone',
+  site_name: 'HyperLinkHub',
   title: 'Nextjs Starter',
   description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
   locale: 'en',
